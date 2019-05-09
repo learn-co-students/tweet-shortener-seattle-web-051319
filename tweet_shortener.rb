c63b6951182a68cd632060d229c0d0da1e678705
bustling-maker-4835 <chopra.chet@gmail.com>
2019-05-09 22:36:36 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,47 @@
-# Write your code here.
+def dictionary
+   dictionary = {
+  "hello" => 'hi',
+  "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  "be" => 'b',
+  "you" => 'u',
+  "at" => "@",
+  "and" => "&"
+  }
+end
+                
+def word_substituter(tweets)
+  new_words = dictionary.keys
+  tweets.split(" ").collect do |word|
+  if new_words.include? word.downcase
+    word = dictionary[word.downcase]
+  else
+    word
+  end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweets)
+  if tweets.length > 140
+    word_substituter(tweets)
+  else
+    tweets
+  end
+end
+
+def shortened_tweet_truncator(tweets)
+  if word_substituter(tweets).length >140
+    return word_substituter(tweets)[0..136] + "..."
+  else
+    return tweets
+  end
+end
